25c1ea75a8827ba8da462be918e9a8358d000930
gsenno <gsenno@gsenno-HP-Laptop-15-da1xxx>
2019-08-18 21:33:08 +0200

    small fix

--- sequential-bell-with-comm/src/optimalRandom.py
+++ sequential-bell-with-comm/src/optimalRandom.py
@@ -35,14 +35,15 @@ if __name__ == '__main__':
     Kplus=np.cos(epsilon)*plus*plus.dag()+np.sin(epsilon)*minus*minus.dag()
     Kminus=-np.cos(epsilon)*minus*minus.dag()+np.sin(epsilon)*plus*plus.dag()
     
-    alice1Effects.append([Kplus*Kplus.dag(),Kminus*Kminus.dag()])
+    alice1Effects.append([Kplus,Kminus])
     
     alice2blochVectors=[[0,0,1],[1,0,0]]
     alice2Observables = list(map(lambda bloch : createQubitObservable(bloch),alice2blochVectors))
     alice2Effects = list(map(lambda qubitObservable : projectorsForQubitObservable(qubitObservable),alice2Observables))
-    trineAlice2 = [[0,0,np.cos(2*np.pi/3)],[np.sin(2*np.pi/3),0,np.cos(2*np.pi/3)],[np.sin(4*np.pi/3),0,np.cos(4*np.pi/3)]]
+    trineAlice2 = [[0,0,1],[np.sin(2*np.pi/3),0,np.cos(2*np.pi/3)],[np.sin(4*np.pi/3),0,np.cos(4*np.pi/3)]]
+    paulies=[qt.sigmax(),qt.sigmay(),qt.sigmaz()]
     alice2Effects.append(list(map(lambda bloch : 
-                           effectForQubitPovm(1/3, createQubitObservable(bloch)),trineAlice2)))
+                           effectForQubitPovm(1/3, sum([paulies[i]*bloch[i] for i in range(0,3)])),trineAlice2)))
     
     mu=np.arctan(np.sin(2*epsilon))
     bobUnBlochVectors = [[np.sin(mu),0,np.cos(mu)],[-np.sin(mu),0,np.cos(mu)]]
@@ -52,15 +53,15 @@ if __name__ == '__main__':
     psi=createMaxEntState(2)
     rho=psi*psi.dag()
     expectedCorrelations={}
-    for x1 in range(2):
-        for a1 in range(2):
+    for x1,x2,y in product(range(2),range(3),range(2)):
+        for a1,a2,b in product(range(alice1outputs[x1]),range(alice2outputs[x2]),range(bobOutputs[y])):
+            px1a1=(qt.tensor(alice1Effects[x1][a1].dag()*alice1Effects[x1][a1],qt.qeye(2))*rho).tr()
             postMeasrmntState = qt.tensor(alice1Effects[x1][a1],qt.qeye(2))*rho*(qt.tensor(alice1Effects[x1][a1],qt.qeye(2))).dag()
-            for x2,y in product(range(3),range(2)):
-                for a2,b in product(range(alice2outputs[x2]),range(bobOutputs[y])):
-                    expectedCorrelations[(x1,x2),y,(a1,a2),b]=(qt.tensor(alice2Effects[x2][a2],bobEffects[y][b])*
+            expectedCorrelations[(x1,x2),y,(a1,a2),b]=(qt.tensor(alice2Effects[x2][a2],bobEffects[y][b])*
                                                            postMeasrmntState).tr().real
-    expectedBehaviour = Behaviour(scenario,expectedCorrelations)
     
+    expectedBehaviour = Behaviour(scenario,expectedCorrelations)
+
     polytope = BellPolytopeWithOneWayCommunication(SequentialBellPolytope(scenario))                
     
     print(polytope.contains(expectedBehaviour.getProbabilityList()))
